0bc732f5a04590904d9c83cefb733a606296cf1a
Tech Student <razaqhammed36@gmail.com>
2021-01-20 18:42:53 -0500

    Added the ability to open files and some path checks.

--- repo.py
+++ repo.py
@@ -1,18 +1,54 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 import os
 
 repo = Blueprint("Repo", __name__, template_folder="templates", static_folder="static")
 
-@repo.route("/<username>/<repo>")
-def openRepo(username, repo):
-	if username in os.listdir("repos"):
-		if repo in os.listdir(os.path.join("repos", username)):
-			path = os.path.join("repos", username, repo)
-			files = os.listdir(path)
+@repo.route("/<path:path>")
+def openRepoOrUser(path):
+	if path.endswith("/"):
+		path = path[:len(path) - 1]
 
-			return render_template("repo.html", username=username, repo_name=repo, files=files)
+	pathList = path.split("/")
+	if len(pathList) == 2:
+		username = pathList[0]
+		if username in os.listdir("repos"):
+			repo = pathList[1]
+			repos = os.path.join("repos", username)
+			if repo in os.listdir(repos):
+				path = os.path.join("repos", username, repo)
+				files = os.listdir(path)
+				return render_template('repo.html', username=username, repo_name=repo, files=files)
+			else:
+				return "Repository doesn't exist."
+		else:
+			return "User doesn't exist."
+	
+	if len(pathList) == 3:
+		username = pathList[0]
+		if username in os.listdir("repos"):
+			repo = pathList[1]
+			repos = os.path.join("repos", username)
+			if repo in os.listdir(repos):
+				filename = pathList[2]
+				files = os.path.join(repos, repo)
+				if filename in os.listdir(files):
+					filePath = os.path.join("repos", username, repo, filename)
+					file = open(filePath, "r")
+					data = file.read()
+					file.close()
 
-		return "Repository doesn't exist."
+					file = {
+						'name': filename,
+						'content': data
+					}
 
-	return "User doesn't exist."
+					return render_template("file.html", file=file)
+				else:
+					return "File doesn't exist."
+			else:
+				return "Repository doesn't exist."
+		else:
+			return "User doesn't exist."
+	
+	return redirect(url_for('home'))
