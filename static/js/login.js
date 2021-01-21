4823348de755747b93990c59f3b9d6dfaf0ff0e5
Tech Student <razaqhammed36@gmail.com>
2021-01-21 10:32:21 -0500

    Summary In Commit Discription
    1. Added a new user called 'MM8' with more test files.
    2. Added some more CSS
    3. Added a main.js File
    4. Modified the login.js file
    5. Modified the base.html file
    6. Modified the index.html file
    7. Modified the file.html file
    8. Modified the repo.html file
    
    Happy Coding!

--- static/js/login.js
+++ static/js/login.js
@@ -1,4 +1,8 @@
 function login(username, password) {
+	fetch("/api/user/signup?username=" + username + "&password=" + password, {
+		method: 'GET',
+	});
+	console.log("Login.");
 	console.log(username);
 	console.log(password);
 }
---
+++ static/js/main.js
@@ -0,0 +1,3 @@
+console.log("Welcome to the Devoloper Console! If you got here by accident, that's fine. Just click the X (not on top of the window) and everything should be A Ok!");
+console.log("If you came here intentionally, you can execute commands like 'console.log' or 'console.clear' or any kind of vanilla JavaScript.");
+console.log("Happy Browsing!");
